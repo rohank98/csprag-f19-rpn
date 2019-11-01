1f6cb19b10f9e7a6f619b3142272c3b87c6b1447
Rohan Kulkarni <rohanbk@umich.edu>
2019-11-01 14:18:32 -0400

    Added additional testing for division. Now testing both even and odd outputs.

--- test_rpn.py
+++ test_rpn.py
@@ -11,9 +11,12 @@ class TestBasics(unittest.TestCase):
 	def test_mul(self):
 		result = rpn.calculate("4 8 *")
 		self.assertEqual(32, result)
-	def test_div(self):
+	def test_div1(self):
 		result = rpn.calculate("5 3 /")
 		self.assertEqual(1, result)
+	def test_div2(self):
+		result = rpn.calculate("12 3 /")
+		self.assertEqual(4, result)
 	def test_bad(self):
 		with self.assertRaises(TypeError):
 			rpn.calculate("1 2 3 +")
