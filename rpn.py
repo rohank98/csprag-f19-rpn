cf8de58aa33738ea91b3a2de3eade7a7c6611eb6
Rohan Kulkarni <rohanbk@umich.edu>
2019-11-01 14:04:44 -0400

    Remove handwritten operator functions for operator equivalents

--- rpn.py
+++ rpn.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
 
-def add(a, b):
-	return a + b
-
-def sub(a, b):
-	return a - b
+import operator
 
 operators = {
-	'+': add,
-	'-': sub,
+	'+': operator.add,
+	'-': operator.sub,
 }
 
 def calculate(string):
