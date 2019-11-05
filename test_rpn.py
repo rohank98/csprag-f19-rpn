c404a569b20d4a9cd32c4af85fcc8f62ae229ab7
Rohan Kulkarni <rohanbk@umich.edu>
2019-11-05 02:34:18 -0500

    Added testing for exponentiation operator (^)

--- test_rpn.py
+++ test_rpn.py
@@ -17,6 +17,9 @@ class TestBasics(unittest.TestCase):
 	def test_div2(self):
 		result = rpn.calculate("12 3 /")
 		self.assertEqual(4, result)
+	def test_exp(self):
+		result = rpn.calculate("3 4 ^")
+		self.assertEqual(81, result)
 	def test_bad(self):
 		with self.assertRaises(TypeError):
 			rpn.calculate("1 2 3 +")
